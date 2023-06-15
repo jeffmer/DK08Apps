825a5c6db7f1747223c753d5da2780fb0af10481
jeffmer <jeffreymagee@gmail.com>
2023-06-15 15:19:12 +0100

    update reboot.js

--- apps/boot/reboot.js
+++ apps/boot/reboot.js
@@ -1,6 +1,14 @@
 
+function do_reboot(){
+  E.showMessage("Rebooting\n NOW");
+  setInterval(()=>{
+    if (!BTN1.read()) E.reboot();
+    else E.showMessage("Release\n BTN");
+  },1000);
+}
+
 DK08.on("dbltouch",(t)=>{
-    E.reboot();
+    do_reboot();
   });
 
 setTimeout(()=>{
